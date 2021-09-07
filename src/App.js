5c780f0f417844a9cd1f92052d09bba370ca35d1
safad <safadeldoul@gmail.com>
2021-09-07 12:19:47 +0100

    organized code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import './App.css';
-import News from './components/News'
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
+import "./App.css";
+import News from "./components/News";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
 function App() {
-
-
   return (
-    <div className="App" > 
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-
-    <News />
-    </MuiPickersUtilsProvider>
-
+    <div className="App">
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <News />
+      </MuiPickersUtilsProvider>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
