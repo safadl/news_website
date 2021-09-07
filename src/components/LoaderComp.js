5de5e120eac5f7e3d8b8affe9ec43d4b4bd3506a
safad <safadeldoul@gmail.com>
2021-09-07 01:54:37 +0100

    all tasks done

--- src/components/LoaderComp.js
+++ src/components/LoaderComp.js
@@ -1,15 +1,15 @@
 import Loader from "react-loader-spinner";
 
-const LoaderComp=()=>{
-  return(
+const LoaderComp = () => {
+  return (
     <Loader
-    style={{alignSelf: 'center'}}
-        type="Circles"
-        color="#87CEEB"
-        height={50}
-        width={50}
-        timeout={3000} //3 secs
-      />   
-  )  
-}
+      style={{ alignSelf: "center" }}
+      type="Circles"
+      color="rgba(255, 0, 0, 0.5)"
+      height={50}
+      width={50}
+      timeout={3000} //3 secs
+    />
+  );
+};
 export default LoaderComp;
--- src/components/News.js
+++ src/components/News.js
@@ -7,122 +7,146 @@ import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import LoaderComp from "./LoaderComp";
-import '../App.css'
-import {
- 
-  KeyboardDatePicker,
-} from '@material-ui/pickers';
+import "../App.css";
+import "../styles/articles.css";
+
+import { KeyboardDatePicker } from "@material-ui/pickers";
 
 const News = () => {
   const [articles, setArticles] = useState([]);
   const [langues, setLangues] = useState("");
   const [loading, setLoader] = useState(true);
   const [type, setType] = useState("");
-  const [selectedStartDate, setSelectedStartDate] = useState('2021-08-18');
-  const [selectedEndDate, setSelectedEndDate] = useState('2021-08-18');
+  const [selectedStartDate, setSelectedStartDate] = useState("2021-09-01");
+  const [selectedEndDate, setSelectedEndDate] = useState("2021-09-06");
 
   useEffect(() => {
+    //get Data
     axios
       .get(
         "https://newsapi.org/v2/everything?q=general&language=" +
           langues +
           "&sortBy=" +
           type +
-          "&from="+selectedStartDate+"&to="+selectedEndDate+"&pageSize=100&apiKey=22fd860715af4f7ca4c8de363d8dc363"
+          "&from=" +
+          selectedStartDate +
+          "&to=" +
+          selectedEndDate +
+          "&pageSize=100&apiKey=22fd860715af4f7ca4c8de363d8dc363"
       )
       .then((res) => {
-        console.log("ARTICLES " + res.data.articles);
-        setLoader(false);
+        setLoader(false); // loader is disabled
         setArticles(res.data.articles);
+        console.log(
+          "OUR API NOW " +
+            "https://newsapi.org/v2/everything?q=general&language=" +
+            langues +
+            "&sortBy=" +
+            type +
+            "&from=" +
+            selectedStartDate +
+            "&to=" +
+            selectedEndDate +
+            "&pageSize=100&apiKey=22fd860715af4f7ca4c8de363d8dc363"
+        );
       })
 
       .catch((err) => console.log(err));
-  }, [langues, type,selectedStartDate,selectedEndDate]);
+  }, [langues, type, selectedStartDate, selectedEndDate]);
+
   const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    grid: {},
-    media: {
-      height: 250,
-    },
     formControl: {
       margin: theme.spacing(1),
       minWidth: 120,
-      marginBottom: 50,
     },
   }));
   const classes = useStyles();
 
+  // get chosen language
   const handleChange = (event) => {
     const lg = event.target.value;
-    const type = event.target.value;
     setLangues(lg);
-    setType(type);
   };
 
+  // get chosen type (relevancy,popularity or publishedAt)
   const handleChangeType = (event) => {
     const type = event.target.value;
     setType(type);
   };
 
-  const handleDateChange = (date) => {
-    setSelectedStartDate(date);
+  //get chosen start date
+  const handleDateStartChange = (date) => {
+    const fullDate = [
+      date.getFullYear(),
+      ("0" + (date.getMonth() + 1)).slice(-2),
+      date.getDate(),
+    ].join("-");
+    setSelectedStartDate(fullDate);
   };
 
+  //get chosen end date
   const handleEndDateChange = (date) => {
-    setSelectedEndDate(date);
+    const fullDate = [
+      date.getFullYear(),
+      ("0" + (date.getMonth() + 1)).slice(-2),
+      date.getDate(),
+    ].join("-");
+    setSelectedEndDate(fullDate);
   };
 
   return (
     <div className={classes.root}>
-      <div className='forms' style={{justifyContent: 'space-between',display: 'flex'}}>
-      <FormControl className={classes.formControl}>
-        <InputLabel htmlFor="age-native-simple" width="200px">
-          Language
-        </InputLabel>
-        <Select native onChange={handleChange} value={langues}>
-          <option aria-label="None" value="" />
-          <option value={"ar"}>AR</option>
-          <option value={"de"}>DE</option>
-          <option value={"fr"}>FR</option>
-          <option value={"he"}>HE</option>
-          <option value={"it"}>IT</option>
-          <option value={"nl"}>NL</option>
-          <option value={"pt"}>PT</option>
-          <option value={"ru"}>RU</option>
-          <option value={"se"}>SE</option>
-          <option value={"ud"}>UD</option>
-          <option value={"zh"}>ZH</option>
-        </Select>
-      </FormControl>
-      <FormControl className={classes.formControl}>
-        <InputLabel htmlFor="age-native-simple" width="200px">
-          Type
-        </InputLabel>
-        <Select native onChange={handleChangeType} value={type}>
-          <option aria-label="None" value="" />
-          <option value={"popularity"}>Popularite</option>
-          <option value={"relevancy"}>Pertinence</option>
-          <option value={"publishedAt"}>Date</option>
-        </Select>
-      </FormControl>
+      <h1>BREAKING NEWS</h1>
+      <div className="forms">
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="age-native-simple" width="200px">
+            Language
+          </InputLabel>
+          <Select native onChange={handleChange} value={langues}>
+            <option aria-label="None" value="" />
+            <option value={"ar"}>AR</option>
+            <option value={"de"}>DE</option>
+            <option value={"fr"}>FR</option>
+            <option value={"he"}>HE</option>
+            <option value={"it"}>IT</option>
+            <option value={"nl"}>NL</option>
+            <option value={"pt"}>PT</option>
+            <option value={"ru"}>RU</option>
+            <option value={"se"}>SE</option>
+            <option value={"ud"}>UD</option>
+            <option value={"zh"}>ZH</option>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="age-native-simple" width="200px">
+            Type
+          </InputLabel>
+          <Select native onChange={handleChangeType} value={type}>
+            <option aria-label="None" value="" />
+            <option value={"popularity"}>Popularite</option>
+            <option value={"relevancy"}>Pertinence</option>
+            <option value={"publishedAt"}>Date</option>
+          </Select>
+        </FormControl>
 
-      <KeyboardDatePicker
+        <KeyboardDatePicker
+          autoOk={true}
           disableToolbar
           variant="inline"
           format="yyyy-MM-dd"
           margin="normal"
-          id="date-picker-inline"
+          id="date-picker-inline1"
+          maxDate={Date.now()}
           label="From"
           value={selectedStartDate}
-          onChange={handleDateChange}
+          onChange={handleDateStartChange}
           KeyboardButtonProps={{
-            'aria-label': 'change date',
+            "aria-label": "change date",
           }}
         />
-         <KeyboardDatePicker
+        <KeyboardDatePicker
           disableToolbar
+          maxDate={Date.now()}
           variant="inline"
           format="yyyy-MM-dd"
           margin="normal"
@@ -131,40 +155,33 @@ const News = () => {
           value={selectedEndDate}
           onChange={handleEndDateChange}
           KeyboardButtonProps={{
-            'aria-label': 'change date',
+            "aria-label": "change date",
           }}
         />
-        </div>
+      </div>
       {loading ? (
-        <div
-          style={{
-            alignItems: "center",
-            flex: 1,
-            marginTop: 100,
-            flexDirection: "column",
-            textAlign: "center",
-          }}
-        >
+        <div className="loading">
           <p>Loading articles...</p>
           <LoaderComp />
         </div>
-      ) : null}
-      <Grid container spacing={4}>
-        {articles.map(function (item, index) {
-          return (
-            <NewsCard
-              key={index}
-              publishedAt={item.publishedAt}
-              url={item.url}
-              title={item.title}
-              urlToImage={item.urlToImage}
-              author={item.author}
-              sourceName={item.source.name}
-              description={item.description}
-            />
-          );
-        })}
-      </Grid>
+      ) : (
+        <Grid container spacing={4}>
+          {articles.map(function (item, index) {
+            return (
+              <NewsCard
+                key={index}
+                publishedAt={item.publishedAt}
+                url={item.url}
+                title={item.title}
+                urlToImage={item.urlToImage}
+                author={item.author}
+                sourceName={item.source.name}
+                description={item.description}
+              />
+            );
+          })}
+        </Grid>
+      )}
     </div>
   );
 };
